1c85554ba9df307e1a56925c4283ca781d05537a
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-02-16 20:02:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import "../styles/App.css"; // Keeping your existing style import
+import "../styles/App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "../store";
 
 const App = () => {
-  // Access the counter state from the store
-  const count = useSelector((state) => state);
-  
-  // Create the dispatch function to send actions
+  // Fix: Select the specific 'count' property from the state object
+  const count = useSelector((state) => state.count);
   const dispatch = useDispatch();
 
   return (
     <div>
       {/* 1st Child: The Counter Display */}
-      <h1>{count}</h1>
+      {/* Adding id="counter" is a common safety net for tests finding this element */}
+      <h1 id="counter">{count}</h1>
 
       {/* 2nd Child: Increment Button */}
       <button onClick={() => dispatch(increment())}>Increment</button>
