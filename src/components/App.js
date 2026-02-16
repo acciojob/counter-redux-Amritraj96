9f8cd7a749a165760ea2b9e8600feeba836fb255
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-02-16 19:53:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
 import React from "react";
-import './../styles/App.css';
+import "../styles/App.css"; // Keeping your existing style import
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../store";
 
 const App = () => {
+  // Access the counter state from the store
+  const count = useSelector((state) => state);
+  
+  // Create the dispatch function to send actions
+  const dispatch = useDispatch();
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* 1st Child: The Counter Display */}
+      <h1>{count}</h1>
 
-export default App
+      {/* 2nd Child: Increment Button */}
+      <button onClick={() => dispatch(increment())}>Increment</button>
+
+      {/* 3rd Child: Decrement Button */}
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+};
+
+export default App;
